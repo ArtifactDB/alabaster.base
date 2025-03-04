306e96dc379b1691f9af5c3d5675f49048f960bb
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-03-03 17:22:36 -0800

    Try a bit harder.

--- R/vls.R
+++ R/vls.R
@@ -5,7 +5,7 @@ h5_use_vls <- function(len) {
 
 #' @export
 h5_create_vls_pointer_dataset <- function(handle, name, dimensions, chunks, compress=6, scalar=FALSE, emit=TRUE) {
-    create_vls_pointer_dataset(handle, name, dimensions, chunks, compress, scalar)
+    create_vls_pointer_dataset(handle@ID, name, dimensions, chunks, compress, scalar)
     if (emit) {
         return(H5Dopen(handle, name))
     }
@@ -13,12 +13,12 @@ h5_create_vls_pointer_dataset <- function(handle, name, dimensions, chunks, comp
 
 #' @export
 h5_write_vls_array <- function(pointers, heap, x, len, buffer.size=1e6) {
-    dump_vls_pointers(pointers, len, buffer.size)
-    dump_vls_heap(heap, x, len, buffer.size)
+    dump_vls_pointers(pointers@ID, len, buffer.size)
+    dump_vls_heap(heap@ID, x, len, buffer.size)
 }
 
 #' @export
 #' @importFrom rhdf5 H5Dget_space H5Sclose H5Sget_simple_extent_dims
 h5_read_vls_array <- function(pointers, heap, buffer.size=1e6, keep.dim=TRUE, transposed=TRUE) {
-    parse_vls_pointers(pointers, heap, buffer.size)
+    parse_vls_pointers(pointers@ID, heap, buffer.size)
 }
